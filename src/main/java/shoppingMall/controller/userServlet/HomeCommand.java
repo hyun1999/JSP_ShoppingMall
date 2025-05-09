528d6debb7510aced917295108cddc5a69f1efa7
parkhyunseo <dazzle1999@naver.com>
2025-05-10 00:57:42 +0900

    Feat: 검색창 카테고리 반영

--- src/main/java/shoppingMall/controller/userServlet/HomeCommand.java
+++ src/main/java/shoppingMall/controller/userServlet/HomeCommand.java
@@ -4,12 +4,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import shoppingMall.controller.Command;
+import shoppingMall.domain.Category;
+import shoppingMall.domain.Product;
+import shoppingMall.service.CategoryService;
+import shoppingMall.service.ProductService;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HomeCommand implements Command {
+    private final ProductService productService = new ProductService();
+    private final CategoryService categoryService = new CategoryService();
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        List<Product> productList = productService.getAllProducts();
+        List<Category> categoryList = categoryService.getAllCategories();
+        request.setAttribute("categoryList", categoryList);
+        request.setAttribute("productList", productList);
+        request.getRequestDispatcher("/indexForm.jsp").forward(request, response);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,28 +1,4 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%
-    String userName = (String) session.getAttribute("userName");
+    response.sendRedirect(request.getContextPath() + "/home.do");
 %>
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <title>Coupang</title>
-    <link rel="stylesheet" href="css/style.css?v=2">
-</head>
-<body>
-
-<jsp:include page="/common/header.jsp" />
-
-<main>
-    <% if (userName != null) { %>
-    <div class="welcome"><strong><%= userName %></strong> 님 반갑습니다.</div>
-    <% } %>
-
-    <jsp:include page="/common/menu.jsp" />
-    <jsp:include page="/common/products.jsp" />
-</main>
-
-<jsp:include page="/common/footer.jsp" />
-
-</body>
-</html>
